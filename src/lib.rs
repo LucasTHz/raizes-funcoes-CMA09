ba125e36be44d7cfa616e0b9790436b7b595fd5a
LucasTHz <lucasthomaz58@gmail.com>
2022-09-17 14:49:01 -0300

    formatando e adicionando comentarios

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,55 @@
+// A função utilizada é f(x) = (x/2)² - sin x
+// Calculo realizado através do método da falsa posicao
 pub mod false_position {
-  pub fn fa(a: f32) -> f32 {
-    let funcao_a = f32::powf(a / 2.0, 2.0) - a.sin();
-    return funcao_a;
-  }
+    pub fn fa(a: f32) -> f32 {
+        let funcao_a = f32::powf(a / 2.0, 2.0) - a.sin();
+        return funcao_a;
+    }
 
-// método que calcula o F(b)
-  pub fn fb(b: f32) -> f32 {
-      let funcao_b = f32::powf(b / 2.0, 2.0) - b.sin();
-      return funcao_b;
-  }
+    // método que calcula o F(b)
+    pub fn fb(b: f32) -> f32 {
+        let funcao_b = f32::powf(b / 2.0, 2.0) - b.sin();
+        return funcao_b;
+    }
 
-  pub fn xk(a: f32, b: f32) -> f32 {
-      let dividendo = (a * fb(b)) - (b * fa(a));
-      return dividendo / (fb(b) - fa(a));
-  }
+    // método que calcula o xk
+    pub fn xk(a: f32, b: f32) -> f32 {
+        let dividendo = (a * fb(b)) - (b * fa(a));
+        return dividendo / (fb(b) - fa(a));
+    }
 
-  pub fn fxk(xk: f32) -> f32 {
-      let funcao_xk: f32 = f32::powf(xk / 2.0, 2.0) - xk.sin();
-      return funcao_xk;
-  }
+    // método que calcula o F(xk)
+    pub fn fxk(xk: f32) -> f32 {
+        let funcao_xk: f32 = f32::powf(xk / 2.0, 2.0) - xk.sin();
+        return funcao_xk;
+    }
 }
 
 pub mod bissecao {
-  // A função utilizada é f(x) = (x/2)² - sin x
-  // Calculo realizado através do método da bissecção
+    // A função utilizada é f(x) = (x/2)² - sin x
+    // Calculo realizado através do método da bissecção
 
-  // método que calcula o F(a)
-  pub fn fa(a: f32) -> f32 {
-      let funcao_a = f32::powf(a / 2.0, 2.0) - a.sin();
-      return funcao_a;
-  }
+    // método que calcula o F(a)
+    pub fn fa(a: f32) -> f32 {
+        let funcao_a = f32::powf(a / 2.0, 2.0) - a.sin();
+        return funcao_a;
+    }
 
-  // método que calcula o F(b)
-  pub fn fb(b: f32) -> f32 {
-      let funcao_b = f32::powf(b / 2.0, 2.0) - b.sin();
-      return funcao_b;
-  }
+    // método que calcula o F(b)
+    pub fn fb(b: f32) -> f32 {
+        let funcao_b = f32::powf(b / 2.0, 2.0) - b.sin();
+        return funcao_b;
+    }
 
-  // método que calcula o xk1.5
-  pub fn xk(a: f32, b: f32) -> f32 {
-      let xk = (a + b) / 2.0;
-      return xk;
-  }
+    // método que calcula o xk1.5
+    pub fn xk(a: f32, b: f32) -> f32 {
+        let xk = (a + b) / 2.0;
+        return xk;
+    }
 
-  // método que calcula o F(xk)
-  pub fn fxk(xk: f32) -> f32 {
-      let funcao_xk: f32 = f32::powf(xk / 2.0, 2.0) - xk.sin();
-      return funcao_xk;
-  }
+    // método que calcula o F(xk)
+    pub fn fxk(xk: f32) -> f32 {
+        let funcao_xk: f32 = f32::powf(xk / 2.0, 2.0) - xk.sin();
+        return funcao_xk;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 mod lib;
 use std::io;
-#[macro_use] extern crate prettytable;
-use prettytable::{Table};
+#[macro_use]
+extern crate prettytable;
+use prettytable::Table;
 
 fn main() {
     println!("Digite o valor de a");
     let mut a = String::new();
-    
+
     io::stdin().read_line(&mut a).expect("Falha ao ler entrada");
-    
+
     println!("Digite o valor de b");
     let mut b = String::new();
-    
+
     io::stdin().read_line(&mut b).expect("Falha ao ler entrada");
-    
+
     let mut a: f32 = a.trim().parse().expect("Por favor, digite um número!");
-    
+
     let mut b: f32 = b.trim().parse().expect("Por favor, digite um número!");
-    
+
     let mut i: f32 = 1.0;
     let tolerancia: f32 = 0.0001;
     let mut table = Table::new();
@@ -25,7 +26,7 @@ fn main() {
 
     let mut a_falsa_position: f32 = a;
     let mut b_falsa_position: f32 = b;
-    
+
     println!("\n Metodo da bissecao");
     table.add_row(row!["a", "b", "xk", "f(xk)", "f(a)", "f(b)"]);
     loop {
@@ -34,15 +35,15 @@ fn main() {
         let fb = lib::bissecao::fb(b); // Importar um modulo com funções
         let fxk = lib::bissecao::fxk(raiz); // Importar um modulo com funções
         table.add_row(row![a, b, raiz, fxk, fa, fb]);
-        
+
         if (fa * fxk) < 0.0 {
             b = raiz;
         } else {
             a = raiz;
         }
-        
+
         i += 1.0;
-        
+
         if i >= 10.0 || f32::abs(fxk) < tolerancia {
             table.printstd();
             break;
@@ -53,11 +54,19 @@ fn main() {
     println!("\n Metodo da Falsa posicao");
     table1.add_row(row!["a", "b", "xk", "f(xk)", "f(a)", "f(b)"]);
     loop {
-        let raiz_falsa = lib::false_position::xk(a_falsa_position, b_falsa_position); // Importar um modulo com funções
-        let fa = lib::false_position::fa(a_falsa_position); // Importar um modulo com funções
-        let fb = lib::false_position::fb(b_falsa_position); // Importar um modulo com funções
-        let fxk = lib::false_position::fxk(raiz_falsa); // Importar um modulo com funções
-        table1.add_row(row![a_falsa_position, b_falsa_position, raiz_falsa, fxk, fa, fb]);
+        // Importar um modulo com funções
+        let raiz_falsa = lib::false_position::xk(a_falsa_position, b_falsa_position);
+        let fa = lib::false_position::fa(a_falsa_position);
+        let fb = lib::false_position::fb(b_falsa_position);
+        let fxk = lib::false_position::fxk(raiz_falsa);
+        table1.add_row(row![
+            a_falsa_position,
+            b_falsa_position,
+            raiz_falsa,
+            fxk,
+            fa,
+            fb
+        ]);
 
         if (fa * fxk) < 0.0 {
             b_falsa_position = raiz_falsa;
